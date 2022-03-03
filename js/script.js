5e1d11432ae6d706e728f735a10a45740dc309f7
Thainunes <thaissnunes@gmail.com>
2022-03-02 22:49:20 -0300

    Inclusão do projeto To Do List com Javascript

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
-let count = 0;
+const form = document.getElementById('task-form');
+const taskList = document.getElementById('tasks');
 
-const CURRENT_NUMBER = document.getElementById('currentNumber');
+form.onsubmit = function (e) {
+    e.preventDefault();
+    const inputField = document.getElementById('task-input');
+    addTask(inputField.value);
+    form.reset();
+};
 
-function increment() {
-    count++;
-    CURRENT_NUMBER.innerHTML = count;
-}
+function addTask(description) {
+    const taskContainer = document.createElement('div');
+    const newTask = document.createElement('input');
+    const taskLabel = document.createElement('label');
+    const taskDescriptionNode = document.createTextNode(description);
 
-function decrement() {
-    count--;
-    CURRENT_NUMBER.innerHTML = count;
+    newTask.setAttribute('type', 'checkbox');
+    newTask.setAttribute('name', description);
+    newTask.setAttribute('id', description);
+
+    taskLabel.setAttribute('for', description);
+    taskLabel.appendChild(taskDescriptionNode);
+
+    taskContainer.classList.add('task-item');
+    taskContainer.appendChild(newTask);
+    taskContainer.appendChild(taskLabel);
+
+    taskList.appendChild(taskContainer);
 }
 
